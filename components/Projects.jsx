576af4043e09b156be576fdc9b5bef170ab61e76
cyberdavinci <ebrimaa2ray@gmail.com>
2022-12-27 17:57:24 +0000

    update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,29 +3,55 @@ import styles from "../styles/Projects.module.css";
 import Image from "next/image";
 const Projects = () => {
   return (
-    <div className={`${styles.workContainer} w-full `}>
-      <div className="w-full">
-        <h2 className="md:text-2xl text-xl md:mx-[15%]">Projects</h2>
+    <div
+      className={`${styles.workContainer} w-full md:flex-none flex flex-col items-center`}
+    >
+      <div className="">
+        <h2 className="text-4xl md:mx-[15%]">Projects</h2>
       </div>
       <div
-        className={`${styles.cardsContainer} mt-10 md:mx-[15%] grid gap-5 md:grid-cols-[repeat(2,1fr)]`}
+        className={`${styles.cardsContainer} mt-10 md:mx-[15%] grid gap-5 md:grid-cols-[repeat(2,2fr)] `}
       >
         <div
-          className={`${styles.card}  rounded-lg flex flex-col items-center justify-center gap-3   md:basis-[300px] flex-grow-0`}
+          className={`${styles.card} max-w-lg  rounded-lg flex flex-col items-center justify-center gap-3    flex-grow-0`}
         >
           {/* <div className={`${styles.card2}`}></div> */}
           {/* <div className={`${styles.card2}`}> */}
-          <div className="iconContainer w-full relative">
+          <div className="iconContainer w-full relative flex justify-center">
             <Image
               src={"/p1.jpg"}
-              fill
-              // height={300}
-              // fill={true}
-              // width={400}
-              // height={0}
-              // sizes={"100vh"}
-              // width="100%"
-              // className={"w-32 h-36"}
+              // fill
+              width={400}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+          <div className="aboutCard">
+            <h2 className=" text-xl font-bold">Pricing Card UX/UI</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              alias voluptatibus nemo eum commodi recusandae.
+            </p>
+            <div className="w-full flex justify-center">
+              <button className="border rounded-lg p-2 mt-4 border-white w-full">
+                See Project
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* thsi */}
+        <div
+          className={`${styles.card} max-w-lg  rounded-lg flex flex-col items-center justify-center gap-3    flex-grow-0`}
+        >
+          {/* <div className={`${styles.card2}`}></div> */}
+          {/* <div className={`${styles.card2}`}> */}
+          <div className="iconContainer w-full relative  flex justify-center">
+            <Image
+              src={"/p1.jpg"}
+              // fill
+              width={400}
+              height={300}
+              className="rounded-lg"
             />
           </div>
           <div className="aboutCard">
